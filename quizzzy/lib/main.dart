dad8cf00dea964f7075c4a885bfcbd2b40cb3244
Karthick47 <63592766+Karthick47v2@users.noreply.github.com>
2022-03-08 23:05:17 +0530

    Created landing page

--- quizzzy/lib/main.dart
+++ quizzzy/lib/main.dart
@@ -1,44 +1,111 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 void main() => runApp(HomePage());
 
 class HomePage extends StatelessWidget {
   const HomePage({ Key? key }) : super(key: key);
 
-  @override
+  @override 
   Widget build(BuildContext context) {
+    Color purpleBdr = Color.fromARGB(255, 93, 0, 155);
+  
     return MaterialApp(
       home: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        backgroundColor: Color.fromARGB(255, 37, 37, 37),
+        body: Stack(
+          alignment: Alignment.center,
           children: [
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                primary: Colors.cyan,
-                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                textStyle: const TextStyle(fontSize: 30),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(50),
-                ),
+            Center(
+              child: Image.asset(
+                'assets/images/Quizzzy.png',
+                width: 229,
+                height: 278,
+                fit: BoxFit.cover,
               ),
-              child: const Text("Teacher"),
-               ),
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                primary: Colors.cyan,
-                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                textStyle: const TextStyle(fontSize: 30),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(50),
+            ),
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Positioned(
+                  bottom: 139,
+                  height: 59,
+                  width: 197,
+                  child: OutlinedButton(
+                    onPressed: () {},
+                    style: OutlinedButton.styleFrom(
+                      side: BorderSide(
+                        color: purpleBdr,
+                        width: 2.0,
+                        style: BorderStyle.solid,
+                      ),
+                      
+                    ),
+                    child: const Text(
+                      "I'm a Teacher",
+                      style: TextStyle(fontFamily: 'Heebo', fontSize: 24, fontWeight: FontWeight.w500, color: Colors.white),
+                      ),
+                  )
                 ),
-              ),
-              child: const Text("Student"),
-               ),
+                Positioned(
+                  bottom: 40,
+                  height: 59,
+                  width: 197,
+                  child: OutlinedButton(
+                    onPressed: () {},
+                    style: OutlinedButton.styleFrom(
+                      side: BorderSide(
+                        color: purpleBdr,
+                        width: 2.0,
+                        style: BorderStyle.solid,
+                      ),
+                    ),
+                    child: const Text(
+                      "I'm a Student",
+                      style: TextStyle(fontFamily: 'Heebo', fontSize: 24, fontWeight: FontWeight.w500, color: Colors.white),
+                      ),
+                  )
+                )
+              ],
+            )
           ],
-        ),)
+        )
+      )
     );
   }
 }
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             ElevatedButton(
+//               onPressed: () {},
+//               style: ElevatedButton.styleFrom(
+//                 primary: Colors.cyan,
+//                 padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+//                 textStyle: const TextStyle(fontSize: 30),
+//                 shape: RoundedRectangleBorder(
+//                   borderRadius: BorderRadius.circular(50),
+//                 ),
+//               ),
+//               child: const Text("Teacher"),
+//                ),
+//             ElevatedButton(
+//               onPressed: () {},
+//               style: ElevatedButton.styleFrom(
+//                 primary: Colors.cyan,
+//                 padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+//                 textStyle: const TextStyle(fontSize: 30),
+//                 shape: RoundedRectangleBorder(
+//                   borderRadius: BorderRadius.circular(50),
+//                 ),
+//               ),
+//               child: const Text("Student"),
+//                ),
+//           ],
+//         ),)
+//     );
+//   }
+// }
