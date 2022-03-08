d0f8af3e207bfaf45b269ed29f27fce43aa2d129
Karthick47 <63592766+Karthick47v2@users.noreply.github.com>
2022-03-09 00:06:27 +0530

    refactored homepage

---
+++ quizzzy/lib/import.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ImportFile extends StatefulWidget {
+  const ImportFile({ Key? key }) : super(key: key);
+
+  @override
+  State<ImportFile> createState() => _ImportFileState();
+}
+
+class _ImportFileState extends State<ImportFile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
--- quizzzy/lib/main.dart
+++ quizzzy/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:quizzzy/import.dart';
+import 'package:quizzzy/login.dart';
+import 'custom_buttons.dart';
 
 void main() => runApp(HomePage());
 
@@ -7,11 +10,10 @@ class HomePage extends StatelessWidget {
 
   @override 
   Widget build(BuildContext context) {
-    Color purpleBdr = Color.fromARGB(255, 93, 0, 155);
   
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 37, 37, 37),
+        backgroundColor: const Color.fromARGB(255, 37, 37, 37),
         body: Stack(
           alignment: Alignment.center,
           children: [
@@ -26,45 +28,22 @@ class HomePage extends StatelessWidget {
             Stack(
               alignment: Alignment.center,
               children: [
-                Positioned(
-                  bottom: 139,
-                  height: 59,
-                  width: 197,
-                  child: OutlinedButton(
-                    onPressed: () {},
-                    style: OutlinedButton.styleFrom(
-                      side: BorderSide(
-                        color: purpleBdr,
-                        width: 2.0,
-                        style: BorderStyle.solid,
-                      ),
-                      
-                    ),
-                    child: const Text(
-                      "I'm a Teacher",
-                      style: TextStyle(fontFamily: 'Heebo', fontSize: 24, fontWeight: FontWeight.w500, color: Colors.white),
-                      ),
-                  )
-                ),
-                Positioned(
-                  bottom: 40,
-                  height: 59,
-                  width: 197,
-                  child: OutlinedButton(
-                    onPressed: () {},
-                    style: OutlinedButton.styleFrom(
-                      side: BorderSide(
-                        color: purpleBdr,
-                        width: 2.0,
-                        style: BorderStyle.solid,
-                      ),
-                    ),
-                    child: const Text(
-                      "I'm a Student",
-                      style: TextStyle(fontFamily: 'Heebo', fontSize: 24, fontWeight: FontWeight.w500, color: Colors.white),
-                      ),
-                  )
-                )
+                CustomOutlinedBtn(
+                  text: "I'm a Teacher", 
+                  bt: 139, 
+                  h: 59,
+                  w: 197, 
+                  context: context, 
+                  route: MaterialPageRoute(builder: (context) => AuthApp(),),
+                  ),
+                CustomOutlinedBtn(
+                  text: "I'm a Student", 
+                  bt: 40, 
+                  h: 59, 
+                  w: 197,
+                  context: context,
+                  route: MaterialPageRoute(builder: (context) => ImportFile(),),
+                  ),
               ],
             )
           ],
@@ -73,39 +52,3 @@ class HomePage extends StatelessWidget {
     );
   }
 }
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         body: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             ElevatedButton(
-//               onPressed: () {},
-//               style: ElevatedButton.styleFrom(
-//                 primary: Colors.cyan,
-//                 padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-//                 textStyle: const TextStyle(fontSize: 30),
-//                 shape: RoundedRectangleBorder(
-//                   borderRadius: BorderRadius.circular(50),
-//                 ),
-//               ),
-//               child: const Text("Teacher"),
-//                ),
-//             ElevatedButton(
-//               onPressed: () {},
-//               style: ElevatedButton.styleFrom(
-//                 primary: Colors.cyan,
-//                 padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-//                 textStyle: const TextStyle(fontSize: 30),
-//                 shape: RoundedRectangleBorder(
-//                   borderRadius: BorderRadius.circular(50),
-//                 ),
-//               ),
-//               child: const Text("Student"),
-//                ),
-//           ],
-//         ),)
-//     );
-//   }
-// }
