d6e575f1d2f8235a11d60dfb6bf741934db2dc01
Karthick47 <63592766+Karthick47v2@users.noreply.github.com>
2022-03-28 00:23:00 +0530

    initialized fast-api

--- docker/app/main.py
+++ docker/app/main.py
@@ -1,14 +1,32 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI                             # for creating APIs
+from pydantic import BaseModel                          # body for POST 
 
 app = FastAPI()
 
-class QuizzzyRequest(BaseModel):
+# body classes for req n' res
+class ModelInput(BaseModel):
     context: str
     answer: str
-class QuizzzyResponse(BaseModel):
+class ModelOutput(BaseModel):
     question: str
 
 model_path = './mcq-model/'
 pretrained_model_name = 'tttttttttttttttttttt'
 
+################################################
+
+# debug
+@app.get('/')
+def index():
+    return {'message': 'hello'}
+
+# req -> context and ans-s,
+# res -> questions    
+#@app.post("/get-questions", response_model=ModelOutput)
+@app.post('/get-questions')
+def model_inference(request: ModelInput):
+    context = request.context
+    answer = request.answer
+
+    question = get_question(context, answer, model ,tokenizer)
+    return ModelOutput(question)
