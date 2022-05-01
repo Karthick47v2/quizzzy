ae19b30c427052c3feb4f9bb032b3c61644ca846
Karthick47v2 <karthicksharma11@gmail.com>
2022-05-01 23:59:18 +0530

    Update firestore trigger

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const { messaging } = require("firebase-admin");
 
 admin.initializeApp();
 
@@ -8,6 +9,7 @@ exports.newUser = functions.auth.user().onCreate((user) => {
   return admin.firestore().collection("users").doc(user.uid).set({
     userType: null,
     isGenerated: false,
+    isWaiting: false,
   });
 });
 
@@ -18,6 +20,16 @@ exports.dltUser = functions.auth.user().onDelete((user) => {
 });
 
 // firestore trigger (send notification when question get generated)
-exports.notifyUser = functions.firestore.document('users/{userID}/{qCol}/0').onCreate((snap, context) => {
-  console.log(context.params.qCol);
+exports.notifyUser = functions.firestore.document('users/{userID}/{qCol}/0').onCreate(async (snap, context) => {
+  // get fcm token
+  const user = await admin.firestore().collection("users").doc(context.params.userID).get();
+  const token = user.data()['token'];
+
+  await admin.messaging().send({
+    notification: {
+      title: "Questionnaire has been generated.",
+      body: `Check out, "${context.params.qCol}" is added`
+    },
+    token: token,
+  });
 });
