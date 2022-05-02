906f7efed6e41649c2335867d344f6f78e188ee9
Karthick47v2 <karthicksharma11@gmail.com>
2022-05-03 02:00:11 +0530

    Add more widgets

--- quizzzy/lib/libs/custom_widgets.dart
+++ quizzzy/lib/libs/custom_widgets.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
 
-// ignore: must_be_immutable
-class CustomNavigatorBtn extends StatelessWidget {
-  final String text;
-  BuildContext? cont;
-  Route? route;
-  VoidCallback? func;
-  Color? clr;
-  Color? txtClr;
+class QuizzzyTemplate extends StatelessWidget {
+  final Widget body;
+  const QuizzzyTemplate({Key? key, required this.body}) : super(key: key);
 
-  CustomNavigatorBtn(
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        return false;
+      },
+      child: MaterialApp(
+        home: Scaffold(
+            resizeToAvoidBottomInset: false,
+            backgroundColor: const Color.fromARGB(255, 37, 37, 37),
+            body: body),
+      ),
+    );
+  }
+}
+
+class QuizzzyNavigatorBtn extends StatelessWidget {
+  final String text;
+  final BuildContext? cont;
+  final Route? route;
+  final VoidCallback? func;
+  final Color? clr;
+  final Color? txtClr;
+
+  const QuizzzyNavigatorBtn(
       {Key? key,
       required this.text,
       this.cont,
@@ -46,13 +65,13 @@ class CustomNavigatorBtn extends StatelessWidget {
   }
 }
 
-// ignore: must_be_immutable
-class CustomTextInput extends StatefulWidget {
+class QuizzzyTextInput extends StatelessWidget {
   final String text;
-  TextEditingController controller;
-  String? Function(String?)? validator;
-  bool isPass;
-  CustomTextInput(
+  final TextEditingController controller;
+  final String? Function(String?)? validator;
+  final bool isPass;
+  final Color cursorClr = const Color.fromARGB(255, 168, 168, 168);
+  const QuizzzyTextInput(
       {Key? key,
       required this.text,
       required this.controller,
@@ -61,21 +80,15 @@ class CustomTextInput extends StatefulWidget {
       : super(key: key);
 
   @override
-  State<CustomTextInput> createState() => _CustomTextInputState();
-}
-
-class _CustomTextInputState extends State<CustomTextInput> {
-  final Color cursorClr = const Color.fromARGB(255, 168, 168, 168);
-  @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 8.0),
       child: TextFormField(
-        obscureText: widget.isPass,
+        obscureText: isPass,
         cursorColor: cursorClr,
         decoration: InputDecoration(
             filled: true,
-            labelText: widget.text,
+            labelText: text,
             labelStyle:
                 const TextStyle(color: Color.fromARGB(255, 212, 212, 212)),
             enabledBorder: const OutlineInputBorder(
@@ -86,38 +99,30 @@ class _CustomTextInputState extends State<CustomTextInput> {
                 borderSide: BorderSide(
               color: cursorClr,
             ))),
-        controller: widget.controller,
-        validator: widget.validator,
+        controller: controller,
+        validator: validator,
         style: const TextStyle(color: Colors.white),
       ),
     );
   }
 }
 
-class PopupModal extends StatefulWidget {
+class PopupModal extends StatelessWidget {
   final double size;
   final List<Widget> wids;
   const PopupModal({Key? key, required this.size, required this.wids})
       : super(key: key);
 
   @override
-  State<PopupModal> createState() => _PopupModalState();
-}
-
-class _PopupModalState extends State<PopupModal> {
-  @override
   Widget build(BuildContext context) {
     return Dialog(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(4),
-      ),
       child: Container(
         color: const Color.fromARGB(255, 13, 13, 15),
-        height: widget.size,
+        height: size,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: widget.wids,
+          children: wids,
         ),
       ),
     );
@@ -150,4 +155,76 @@ void snackBar(BuildContext context, String str, Color clr) {
 
   ScaffoldMessenger.of(context).removeCurrentSnackBar();
   ScaffoldMessenger.of(context).showSnackBar(snackBar);
+}
+
+class QuizzzyCard extends StatelessWidget {
+  final String title;
+  final VoidCallback func;
+  const QuizzzyCard({Key? key, required this.title, required this.func})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: const Color.fromARGB(255, 93, 0, 155),
+      child: InkWell(
+        child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Text(
+              title,
+              style: const TextStyle(
+                fontFamily: 'Heebo',
+                fontSize: 22,
+                fontWeight: FontWeight.w400,
+                color: Color.fromARGB(255, 255, 255, 255),
+              ),
+            )),
+        onTap: func,
+      ),
+    );
+  }
+}
+
+class QuizzzyAns extends StatefulWidget {
+  final String ans;
+  final VoidCallback func;
+  const QuizzzyAns({
+    Key? key,
+    required this.ans,
+    required this.func,
+  }) : super(key: key);
+
+  @override
+  State<QuizzzyAns> createState() => _QuizzzyAnsState();
+}
+
+class _QuizzzyAnsState extends State<QuizzzyAns> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+      child: InkWell(
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          decoration: const BoxDecoration(
+            color: Color.fromARGB(94, 153, 0, 255),
+            borderRadius: BorderRadius.all(Radius.circular(24)),
+          ),
+          child: Center(
+            child: Text(
+              widget.ans,
+              style: const TextStyle(
+                  fontFamily: 'Heebo',
+                  fontSize: 22,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+        borderRadius: const BorderRadius.all(Radius.circular(24)),
+        onTap: widget.func,
+      ),
+    );
+  }
 }
