f85177f465d0ca26a69f908e9d0cf0a8f98de451
Karthick47v2 <karthicksharma11@gmail.com>
2022-05-14 14:03:45 +0530

    Add mock

--- docker/app/src/firebase_service.py
+++ docker/app/src/firebase_service.py
@@ -12,9 +12,13 @@ class FirebaseService:
     def __init__(self):
         """initialize firebase firestore client.
         """
-        firebase_admin.initialize_app(
-            credentials.Certificate("./secret/serviceAccountKey.json"))
-
+        try:
+            firebase_admin.initialize_app(
+                credentials.Certificate("./secret/serviceAccountKey.json"))
+        # for unit test only
+        except FileNotFoundError:
+            firebase_admin.initialize_app(
+                credentials.Certificate("docker/app/secret/mock-key.json"))
         self._db = firestore.client()
 
     def update_generated_status(self, request, status):
