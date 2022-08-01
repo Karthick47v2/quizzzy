1db50bdcf4dce3d1c90c6cdba45e3a0317fff372
Karthick47v2 <Karthicksharma11@gmail.com>
2022-08-01 21:24:09 +0530

    Remove onetime obs

--- quizzzy/lib/init_controllers.dart
+++ quizzzy/lib/init_controllers.dart
@@ -3,10 +3,8 @@ import 'package:get/get.dart';
 import 'package:quizzzy/controllers/question_list_controller.dart';
 import 'package:quizzzy/controllers/questionnaire_controller.dart';
 import 'package:quizzzy/controllers/user_type_controller.dart';
-import 'package:quizzzy/controllers/utils_controller.dart';
 
 Future initControllers() async {
-  Get.lazyPut(() => UtilsController());
   Get.lazyPut(() => UserTypeController());
   Get.lazyPut(() => QuestionListController());
   Get.lazyPut(() => QuestionnaireController());
