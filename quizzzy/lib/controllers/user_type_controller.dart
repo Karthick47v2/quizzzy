f3ced08ab5224be9c2fc940e6ef1bd88b385fe2a
Karthick47v2 <Karthicksharma11@gmail.com>
2022-08-06 19:23:23 +0530

    change to enum

--- quizzzy/lib/controllers/user_type_controller.dart
+++ quizzzy/lib/controllers/user_type_controller.dart
@@ -1,12 +1,14 @@
 import 'package:get/get.dart';
 
-class UserTypeController extends GetxController {
-  String _userType = "";
+enum UserType { none, teacher, student }
 
-  String get userType => _userType;
+class UserTypeController extends GetxController {
+  UserType _userType = UserType.none;
+
+  UserType get userType => _userType;
 
   setUserType(String str) {
-    _userType = str;
+    _userType = str == "Student" ? UserType.student : UserType.teacher;
     update();
   }
 }
