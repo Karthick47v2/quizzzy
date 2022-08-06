53a773b58578ada0b450a74bc25aef70385dea56
Karthick47v2 <Karthicksharma11@gmail.com>
2022-08-06 19:21:16 +0530

    merge saved_quiz and question_bank

---
+++ quizzzy/lib/screens/question_bank/share_quiz_popup.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
+
+import 'package:quizzzy/custom_widgets/custom_popup.dart';
+import 'package:quizzzy/service/fs_database.dart';
+import 'package:quizzzy/theme/font.dart';
+import 'package:quizzzy/theme/palette.dart';
+
+class ShareQuizPopup extends StatelessWidget {
+  final String quizName;
+  const ShareQuizPopup({Key? key, required this.quizName}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var code = FirestoreService().user!.uid + '-' + quizName;
+    Share.share(code);
+    return CustomPopup(size: 150.0, wids: [
+      Text(
+        "Share quiz",
+        style: TextStyle(
+          fontFamily: fontFamily,
+          fontSize: 19,
+          fontWeight: Font.regular,
+          color: Palette.font,
+        ),
+        textAlign: TextAlign.center,
+      ),
+      Text(
+        "Code: $code",
+        style: TextStyle(
+          fontFamily: fontFamily,
+          fontSize: 19,
+          fontWeight: Font.regular,
+          color: Palette.font,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    ]);
+  }
+}
---
+++ quizzzy/lib/screens/question_bank/wipe_data.dart
@@ -0,0 +1,20 @@
+import 'package:get/get.dart';
+import 'package:quizzzy/controllers/questionnaire_controller.dart';
+import 'package:quizzzy/custom_widgets/custom_snackbar.dart';
+import 'package:quizzzy/service/db_model/question_set.dart';
+import 'package:quizzzy/service/fs_database.dart';
+import 'package:quizzzy/theme/palette.dart';
+
+wipeQuestions(String qName) async {
+  List<String> qID = Get.find<QuestionnaireController>().removalList;
+
+  localStorage.delete(qName);
+  Get.back();
+  if (!await FirestoreService().deleteQuestions(qName, qID)) {
+    customSnackBar("Error", "Please try again", Palette.error);
+  } else {
+    Get.find<QuestionnaireController>().resetRemovalList();
+    customSnackBar("Deleting questions...",
+        "Questions will be deleted in few minutes...", Palette.sucess);
+  }
+}
