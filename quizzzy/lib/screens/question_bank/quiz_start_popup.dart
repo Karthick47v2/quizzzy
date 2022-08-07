13236c71ab1952b444a1ea5c8ec9a2f793e3baec
Karthick47v2 <Karthicksharma11@gmail.com>
2022-08-07 20:16:39 +0530

    add illustrations

--- quizzzy/lib/screens/question_bank/quiz_start_popup.dart
+++ quizzzy/lib/screens/question_bank/quiz_start_popup.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
 import 'package:quizzzy/controllers/question_list_controller.dart';
 import 'package:quizzzy/controllers/questionnaire_controller.dart';
 import 'package:quizzzy/controllers/user_type_controller.dart';
 import 'package:quizzzy/custom_widgets/custom_button.dart';
 import 'package:quizzzy/custom_widgets/custom_popup.dart';
+import 'package:quizzzy/custom_widgets/render_img.dart';
 import 'package:quizzzy/screens/question_bank/share_quiz_popup.dart';
 import 'package:quizzzy/screens/questionnaire/student_view.dart';
 import 'package:quizzzy/screens/questionnaire/teacher_view.dart';
@@ -16,11 +18,18 @@ class QuizStartPopup extends StatelessWidget {
   final List<String> questionList =
       Get.find<QuestionListController>().questionList;
   final int idx;
+  final UserType userType = Get.find<UserTypeController>().userType;
   QuizStartPopup({Key? key, required this.idx}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return CustomPopup(size: 150.0, wids: [
+    return CustomPopup(size: 400.0, wids: [
+      RenderImage(
+        path:
+            'assets/images/${userType == UserType.student ? 'quiz' : 'review'}.svg',
+        expaned: false,
+        svgHeight: 200,
+      ),
       Text(
         "Questionnaire: ${questionList[idx]}",
         style: TextStyle(
