78ddcbf155803c0787975bf137da222d8b485328
Karthick47v2 <Karthicksharma11@gmail.com>
2022-08-15 10:33:30 +0530

    fix test

--- quizzzy/test/widget_test/question_bank_test.dart
+++ quizzzy/test/widget_test/question_bank_test.dart
@@ -120,3 +120,31 @@ main() {
     });
   });
 }
+
+// import 'package:flutter/material.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:get/get.dart';
+
+// import '../unit_test/firebase_stub.dart';
+// import 'home_page_test.dart';
+
+// main() {
+//   setUp(() async {
+//     Get.testMode = true;
+//     initStubs();
+//     initHomeStubs();
+//     await initApp();
+//   });
+
+//   testWidgets('Display quiz page elements', (WidgetTester tester) async {
+//     await tapOnWidget(tester);
+
+//     await tester.tap(find.byKey(const Key('btn-quiz-start')));
+//     await tester.pumpAndSettle();
+
+//     expect(find.byType(StudentView), findsOneWidget);
+
+//     expect(find.text(questionnaireSub['question']), findsOneWidget);
+//   });
+
+// }
